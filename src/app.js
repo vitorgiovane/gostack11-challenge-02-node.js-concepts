d88887cb9a8d397a93dc855fc7e40ca79ee8b10c
Vitor Giovane <vitorgpalves@gmail.com>
2020-04-12 20:40:46 -0300

    feat: cria api REST que gerencia repositórios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,85 @@ app.use(cors());
 
 const repositories = [];
 
+const validateRepositoryId = (request, response, next) => {
+  if (!isUuid(request.params.id)) return response.status(400).json({ error: "Invalid repository id." })
+  return next()
+}
+
+const validateRepositoryExistence = (request, response, next) => {
+  const repositoryIndex = repositories.findIndex(repository => repository.id === request.params.id)
+  if (repositoryIndex < 0) return response.status(400).json({ error: "Repository not found." })
+  request.repositoryIndex = repositoryIndex
+  return next()
+}
+
+const isEmpty = obj => {
+  if (obj == null) return true
+  if (Array.isArray(obj) || typeof (obj) === 'string') return obj.length === 0
+  if (typeof (obj) === 'object') return Object.keys(obj).length === 0 && obj.constructor === Object
+  return false
+}
+
+const validateRequestBody = (request, response, next) => {
+  const { title, url, techs } = request.body
+  if (isEmpty(title) || isEmpty(url) || isEmpty(techs)) {
+    return response.status(400).json({ message: "Invalid request body" })
+  }
+  next()
+}
+
+app.use('/repositories/:id', validateRepositoryId, validateRepositoryExistence)
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query
+  if (title) {
+    return response.status(200).json(repositories.filter(
+      repository => repository.title.toLowerCase().includes(title.toLowerCase())
+    ))
+  }
+  return response.status(200).json(repositories)
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", validateRequestBody, (request, response) => {
+  const { title, url, techs } = request.body
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository)
+
+  return response.status(201).json(repository)
+});
+
+app.get("/repositories/:id", (request, response) => {
+  return response.status(200).json(repositories[request.repositoryIndex])
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  let { title, url, techs } = request.body
+
+  title = title || repositories[request.repositoryIndex].title
+  url = url || repositories[request.repositoryIndex].url
+  techs = techs || repositories[request.repositoryIndex].techs
+
+  const { likes } = repositories[request.repositoryIndex]
+  const repository = { id, title, url, techs, likes }
+  repositories[request.repositoryIndex] = repository
+  return response.status(200).json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  repositories.splice(request.repositoryIndex, 1)
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { repositoryIndex } = request
+  const repository = repositories[repositoryIndex]
+  repository.likes++
+
+  repositories[repositoryIndex] = repository
+
+  return response.status(201).json(repository)
 });
 
 module.exports = app;
